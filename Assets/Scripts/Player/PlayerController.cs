3e4737b6b2713b92ed1c9d93435b58cea5343128
trashhcatproductionsltd <51205052+TheGareBear@users.noreply.github.com>
2023-12-12 10:54:59 -0700

    Player look around

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,18 +1,41 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Transform cameraContainer;
+    public float minXLook;
+    public float maxXLook;
+    private float camCurXRot;
+    public float lookSensitivity;
+
+    private UnityEngine.Vector2 mouseDelta;
+
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
-    // Update is called once per frame
-    void Update()
+    void LateUpdate() 
     {
-        
+        CameraLook();
+    }
+
+    void CameraLook() 
+    {
+        camCurXRot += mouseDelta.y * lookSensitivity;
+        camCurXRot = Mathf.Clamp(camCurXRot, minXLook, maxXLook);
+        cameraContainer.localEulerAngles = new UnityEngine.Vector3(-camCurXRot, 0, 0);
+
+        transform.eulerAngles += new UnityEngine.Vector3(0, mouseDelta.x * lookSensitivity, 0);
+    }
+
+    public void OnLookInput(InputAction.CallbackContext context) 
+    {
+        mouseDelta = context.ReadValue<UnityEngine.Vector2>();
     }
 }
